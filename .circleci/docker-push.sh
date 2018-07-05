b58ede2bb69fedff9fba1858e9511c848ebf7fd0
Jibin Mathews <jibinmathews7@gmail.com>
2018-07-05 13:30:34 +0530

    Changing region of ecr

--- .circleci/docker-push.sh
+++ .circleci/docker-push.sh
@@ -2,6 +2,6 @@
 
 . ./.circleci/export-env-vars.sh
 
-eval $(aws ecr get-login --no-include-email --region ap-south-1)
+eval $(aws ecr get-login --no-include-email --region us-west-2)
 
 docker push $IMAGE_NAME:$IMAGE_TAG
