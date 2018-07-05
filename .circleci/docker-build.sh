c9d89dcd2a6f479621cc2cdb134debf74c114b21
Jibin Mathews <jibinmathews7@gmail.com>
2018-07-05 12:46:16 +0530

    Changing contexxt name

--- .circleci/docker-build.sh
+++ .circleci/docker-build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+. ./.circleci/export-env-vars.sh
+
 docker build \
-  -f docker/Dockerfile
-  -t $IMAGE_NAME:$IMAGE_TAG
+  -f docker/Dockerfile \
+  -t $IMAGE_NAME:$IMAGE_TAG \
+  .
   
