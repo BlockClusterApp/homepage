dccfe2ebba20e59ea203a82221884c0b125577ec
Narayan Prusty <arsenal.narayan@gmail.com>
2018-08-26 15:00:55 +0400

    removed import css

--- src/routes/partners/components/Sliders.js
+++ src/routes/partners/components/Sliders.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPiggyBank,
